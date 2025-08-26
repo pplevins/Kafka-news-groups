b3abd3a2a0d356868b38674f5a6228e1935802fb
Pinni Plevinski <shareytora007@gmail.com>
2025-08-26 11:19:15 +0300

    Adding producer_api.py

---
+++ api/producer_api.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+
+from services.producer_service import ProducerService
+
+router = APIRouter(prefix="/kafka", tags=["soldiers"])
+producer_service = ProducerService()
+
+
+@router.get(
+    "/",
+    response_description="Producing news massages and publishing to Kafka",
+)
+async def publish():
+    return await producer_service.publish()
